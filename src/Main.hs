7c0ed64a4017ad9244a862283a1fbc253d9fa66f
Deniz A. Durmaz <denizalpd@ogr.iu.edu.tr>
2019-07-11 02:18:25 +0300

    got rid of Parsec errors by dropping everything before "Simulation:"

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,15 @@ module Main where
 
 import ParseDat
 
+getSimData :: [String] -> [String]
+getSimData [] = []
+getSimData (x:xs) =
+  case x of
+    "Simulation:" -> drop 3 xs
+    _ -> getSimData xs
+
 main :: IO ()
 main = do
-  contents <- lines <$> readFile "test"
+  contents <- getSimData <$> lines <$> readFile "test"
   mapM_ (run parsePlumes) contents
   
